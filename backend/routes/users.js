8be6faa6cd2e404d47b6a24a424cd3dcfacf6502
Oscar Englöf <oscar.englof@hotmail.com>
2021-11-16 10:51:15 +0100

    login user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -67,5 +67,54 @@ router.post(
     }
   }
 );
+router.post(
+  "/login",
+  [
+    body("email", "Invalid input").trim().escape().isLength({
+      min: 1,
+    }),
+  ],
+  async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      // Error messages can be returned in an array using `errors.array()`.
+      console.log("Found validation errors");
+      return res.status(422).json({
+        errors: errors.array(),
+      });
+    } else {
+      // Data from form is valid. Store in database
+      const form = req.body;
+
+      try {
+        var email = req.body.email.toLowerCase();
+        const user = await User.findOne({ email: email });
+
+        // Check if user exist
+        if (user) {
+          match = await user.comparePassword(user, req.body.password);
+
+          // Check if password matches
+          if (match) {
+            const token = user.generateAuthToken();
+            res.send({
+              token: token,
+            });
+          } else {
+            res.status(401).send("email and password doesn't match");
+          }
+        } else {
+          res.status(400).send("User doesn't exist");
+        }
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ message: "Server error" });
+      }
+    }
+  }
+);
 
 module.exports = router;
