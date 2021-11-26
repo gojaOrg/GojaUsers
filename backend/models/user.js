89f51d3fd7f3f2aa9aff0fdc5929262358881ab7
Oscar Englöf <oscar.englof@hotmail.com>
2021-11-26 15:55:37 +0100

    default

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,9 +7,11 @@ const bcrypt = require("bcrypt");
 const UserSchema = new mongoose.Schema({
   profileAudio: {
     type: String,
+    default: "https://goja-audio.s3.eu-north-1.amazonaws.com/1637938469368",
   },
   profilePicture: {
     type: String,
+    default: "https://goja-images.s3.eu-north-1.amazonaws.com/1637938504319",
   },
   userName: {
     type: String,
